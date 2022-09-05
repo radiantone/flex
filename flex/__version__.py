1e3d644c7b199e3ea8d77eeda1a28271c7795800
Darren Govoni <darren@rapidquest.ai>
2022-09-05 14:55:57 -0400

    Updates

--- flex/__version__.py
+++ flex/__version__.py
@@ -5,10 +5,10 @@ __version__.py
 Information about the current version of the package.
 """
 
-__title__ = "emerge"
-__description__ = "Emerge Computational Filesystem."
+__title__ = "flex"
+__description__ = "Flex ORM"
 __version__ = "0.0.1"
 __author__ = "Darren Govoni"
 __author_email__ = "darren@ontrenet.com"
 __license__ = "MIT License"
-__url__ = "https://github.com/radiantone/emerge"
+__url__ = "https://github.com/radiantone/flex"
